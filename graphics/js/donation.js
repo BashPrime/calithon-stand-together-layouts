b19be83daefdf4d0bf14bbe21401ecadad58d345
Evan Taylor <etaylor8086@gmail.com>
2020-06-16 22:37:09 -0700

    Update campaign ID for tiltify api

--- graphics/js/donation.js
+++ graphics/js/donation.js
@@ -1,6 +1,6 @@
 'use strict';
 $(() => {
-	const TILTIFY_CAMPAIGN_ID = 43898; // id for calithon covid relief cmpaign
+	const TILTIFY_CAMPAIGN_ID = 59855; // id for calithon stand together cmpaign
 	const TILTIFY_AUTH_TOKEN = null;
 	let countUp;
 	let currentTotal;
